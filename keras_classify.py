a7e7ceab6be409689e7b30536fd2ba82072904ee
Vinnie Magro <v@vinnie.io>
2017-04-08 23:39:35 -0700

    Work on classifier program, save trained model to file

---
+++ keras_classify.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import numpy as np
+from nltk.tokenize import TweetTokenizer
+from keras.models import load_model
+from collections import defaultdict
+import pickle
+
+model = load_model('model.h5')
+
+data = np.load('tweets.npz')
+max_tweet_length = data['x'].shape[1]
+max_topic_length = data['topics'].shape[1]
+
+vocab_file = open('vocab.pck', 'rb')
+vocabulary = pickle.load(vocab_file)
+
+# print(words)
+print(len(vocabulary), 'unique tokens')
+
+tokenizer = TweetTokenizer(strip_handles=True, reduce_len=True)
+def string_to_array(s, l):
+    tokens = tokenizer.tokenize(s)
+    if len(tokens) > l:
+        tokens = tokens[0:l]
+
+    def index_for_token(token):
+        if token not in vocabulary:
+            return 2
+        return vocabulary[token]
+    tokens = [index_for_token(t) for t in tokens]
+    tokens = np.array(tokens)
+    tokens = np.pad(tokens, (0, l - len(tokens)), 'constant')
+    return tokens
+
+
+topic = "Je soutiens Emmanuel Macron. J'encourage Emmanuel Macron. Emmanuel Macron pour président."
+
+# text = "#Enmarche avec @EmmanuelMacron pour une #diplomatie renouvel\u00e9e, ambitieuse et adapt\u00e9e \u00e0 la donne et aux enjeux du monde d'aujourd'hui https:// twitter.com/enmarcheinter/ status/826580027693228034 \u2026"
+text = "Est-ce pour cela que Pierre Berg\u00e9 s'est subitement mis en marche ?"
+
+topic = string_to_array(topic, max_topic_length).reshape((1, max_topic_length))
+text = string_to_array(text, max_tweet_length).reshape((1, max_tweet_length))
+
+print(topic)
+print(text)
+
+labels = {
+        0: 'agree',
+        1: 'disagree',
+        2: 'unrelated',
+        3: 'neither',
+}
+
+
+prediction = model.predict([topic, text])
+print(prediction)
+
+most_likely = prediction[0].argmax()
+most_likely = labels[most_likely]
+print(most_likely)
--- keras_rnn.py
+++ keras_rnn.py
@@ -3,7 +3,7 @@
 # LSTM for sequence classification in the IMDB dataset
 import numpy as np
 from keras.models import Model
-from keras.layers import Input, Embedding, LSTM, Dense, Dropout
+from keras.layers import Input, Embedding, LSTM, Dense, Dropout, Concatenate
 from keras.layers.convolutional import Conv1D, MaxPooling1D
 from keras.preprocessing import sequence
 from sklearn.model_selection import KFold
@@ -17,11 +17,16 @@ def load_training_data():
     data = np.load('tweets.npz')
     x_train = data['x']
     y_train = data['y']
+    topics_train = data['topics']
     words = data['words']
 
-    return (x_train, y_train, words)
+    return (x_train, topics_train, y_train, words)
 
-(x, y, words) = load_training_data()
+(x, topics, y, words) = load_training_data()
+
+print(x.shape)
+print(topics.shape)
+print(y.shape)
 
 embed_dim = 100
 def load_word_embeddings():
@@ -35,44 +40,67 @@ def load_word_embeddings():
             embedding_matrix[i] = embedding_vector
     return embedding_matrix
 
-# truncate and pad input sequences
-max_review_length = 50
+# make sure we have the right shape for inputs
+max_tweet_length = x.shape[1]
+max_topic_length = topics.shape[1]
+# print(max_tweet_length)
+# print(max_topic_length)
 
 # create the model
 
 embedding_matrix = load_word_embeddings()
 
 def build_model():
-    tweet_input = Input(shape=(max_review_length,), dtype='int32', name='tweet_input')
+    topic_input = Input(shape=(max_topic_length,), dtype='int32', name='topic_input')
+    topic_embedding_1 = Embedding(
+        input_dim=len(words)+1,
+        output_dim=embed_dim,
+        weights=[embedding_matrix],
+        input_length=max_topic_length,
+        trainable=False)(topic_input)
+    topic_dropout_embedding_1 = Dropout(0.2)(topic_embedding_1)
+    topic_lstm_1 = LSTM(100)(topic_dropout_embedding_1)
+    topic_dropout_lstm_1 = Dropout(0.2)(topic_lstm_1)
+
+
+    tweet_input = Input(shape=(max_tweet_length,), dtype='int32', name='tweet_input')
     # use a pretrained embedding, so set the weights and don't let it be trained
     embedding_1 = Embedding(
         input_dim=len(words)+1,
         output_dim=embed_dim,
         weights=[embedding_matrix],
-        input_length=max_review_length,
+        input_length=max_tweet_length,
         trainable=False)(tweet_input)
 
     dropout_embedding_1 = Dropout(0.2)(embedding_1)
     lstm_1 = LSTM(100)(dropout_embedding_1)
     dropout_lstm_1 = Dropout(0.2)(lstm_1)
-    category_output = Dense(4, activation='softmax')(dropout_lstm_1)
 
-    model = Model(inputs=tweet_input, outputs=category_output)
+    # combine the tweet network and the topic network
+    combined = Concatenate()([topic_dropout_lstm_1, dropout_lstm_1])
+    category_output = Dense(4, activation='softmax')(combined)
+
+    model = Model(inputs=[topic_input, tweet_input], outputs=category_output)
 
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     return model
 
+validate = True
+if validate:
+    cvscores = []
+    # k-fold cross validation
+    kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+    for train, test in kfold.split(x, y):
+        model = build_model()
+        model.fit([topics[train], x[train]], y[train], epochs=50, batch_size=64, verbose=True)
+        # Final evaluation of the model
+        scores = model.evaluate([topics[test], x[test]], y[test], verbose=0)
+        print("Accuracy: %.2f%%" % (scores[1]*100))
+        cvscores.append(scores[1]*100)
 
-cvscores = []
-# k-fold cross validation
-kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
-for train, test in kfold.split(x, y):
-    model = build_model()
-    model.fit(x[train], y[train], epochs=50, batch_size=64, verbose=True)
-    # Final evaluation of the model
-    scores = model.evaluate(x[test], y[test], verbose=0)
-    print("Accuracy: %.2f%%" % (scores[1]*100))
-    cvscores.append(scores[1]*100)
+    print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
 
-print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+model = build_model()
+model.fit([topics, x], y, epochs=50, batch_size=64, verbose=True)
+model.save('model.h5')
